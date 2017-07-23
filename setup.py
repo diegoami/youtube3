ffaeb62c654a375324e3588ebb06000d64cb510b
diegoami <diego.amicabile@gmail.com>
2017-07-23 22:52:34 +0200

    Wrong version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='youtube3',
-    version='0.0.2.0',
+    version='0.0.2.1',
     packages=['youtube3'],
     include_package_data=True,
     keywords='youtube wrapper oauth2 v3',
